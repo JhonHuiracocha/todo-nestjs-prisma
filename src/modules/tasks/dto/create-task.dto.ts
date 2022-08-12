bf8095526efc52747466c06941749553dd322f09
Jhon Huiracocha <jhonhuiracocha123456789@gmail.com>
2022-08-12 14:27:58 -0500

    updated dto task: add fields

--- src/modules/tasks/dto/create-task.dto.ts
+++ src/modules/tasks/dto/create-task.dto.ts
@@ -1,1 +1,23 @@
-export class CreateTaskDto {}
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+import { Status } from '@prisma/client';
+
+export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(60, { message: 'The title is too long.' })
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsEnum(Status)
+  @IsOptional()
+  status?: Status;
+}
