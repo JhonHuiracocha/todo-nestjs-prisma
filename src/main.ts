706495fe73cb7db17b33d7f281f1678f9abdf9ad
Jhon Huiracocha <jhonhuiracocha123456789@gmail.com>
2022-08-12 14:05:05 -0500

    environment variable set

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+
+  const globalPrefix: string = 'api';
+  const port: number = config.get<number>('PORT');
+
+  app.enableCors();
+  app.setGlobalPrefix(globalPrefix);
+
+  await app.listen(port, () => {
+    Logger.log(`🚀 Server ready at http://localhost:${port}/${globalPrefix}`);
+  });
 }
 bootstrap();
